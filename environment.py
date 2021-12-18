4e97ac1810cd3a5062aacf387008f59e9137c439
Ryan Anderson <sonorousduck@DESKTOP-HASL5LA.localdomain>
2021-12-18 00:56:14 -0700

    Made a silly mistake and forgot to add epsilon decay

--- environment.py
+++ environment.py
@@ -6,7 +6,9 @@ env = flappy_bird_gym.make("FlappyBird-v0")
 
 state_space = env.observation_space.shape[0]
 agent = DQNAgent()
-epochs = 1000
+epochs = 7000
+max_score = 25 
+
 
 for i in range(epochs):
     state = env.reset()
@@ -20,23 +22,30 @@ for i in range(epochs):
     while not done:
         action = agent.act(state)
         next_state, reward, done, info = env.step(action)
+        next_state = np.reshape(next_state, [1, state_space])
         score += 1
 
         if done:
             reward -= 100
-        #
+        else:
+            reward += 0.5
+
+
         # env.render()
         # time.sleep(1/30)
 
         agent.remember(state, action, reward, next_state, done)
         state = next_state
 
-        if score >= 1000:
+        if score > max_score and i > 10:
+            max_score = score
+            print(max_score)
             print(f"Yo, this is getting pretty good")
-            agent.model.save_weights(f"{score}.hdf5")
-
-        agent.learn()
-
+            agent.model.save_weights(f"{i}.hdf5")
+        
+        if i > 10:
+            agent.learn()
+            agent.decreaseEpsilon()
 
 
 
